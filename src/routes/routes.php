17a46858e4f6a02f29894fb2d7c0e558421e4070
Fabian <fh@moonshiner.at>
2017-07-06 14:42:51 +0200

    add compatibility for laravel 5.3

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -2,14 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('cms')
-    ->middleware('api')
-    ->namespace('Moonshiner\Cms\Http\Controllers')
-    ->group(function () {
+Route::group(['prefix' =>'cms', 'middleware' => 'api', 'namespace' => 'Moonshiner\Cms\Http\Controllers'], function () {
         Route::get('config', 'BackendController@config');
         Route::resource('posts', 'BackendController');
         Route::post('file', 'BackendController@saveFile'); 
     });
-Route::namespace('Moonshiner\Cms\Http\Controllers')->group(function(){
+Route::group(['namespace' => 'Moonshiner\Cms\Http\Controllers'], function(){
     Route::get('/{slug?}', 'FrontendController@index')->where('slug', '(.*)');
 }); 
